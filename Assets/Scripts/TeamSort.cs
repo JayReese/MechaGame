0d072a5c53de154a42c6711218fc05d589dbdb48
ChaRRoss <charlesjross92@gmail.com>
2017-05-16 15:11:22 -0500

    Team Select
    So what I have is a working spawn off of a slider. As im not sure as to
    how we're doing the multiplayer, I did a single person select.

---
+++ Assets/Scripts/TeamSort.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TeamSort : MonoBehaviour {
+    [SerializeField]
+    Slider teamSlider;
+    [SerializeField]
+    GameObject team1Member;
+    [SerializeField]
+    GameObject team2Member;
+    [SerializeField]
+    Canvas teamSelectCanvas;
+    [SerializeField]
+    Camera teamSelectCamera;
+    public Transform team1Spawn;
+    public Transform team2Spawn;
+
+    public void SetTeamSliderPosition()
+    {
+        if (teamSlider.value > .5)
+        {
+            teamSlider.value = 0;
+        }
+        else
+        {
+            teamSlider.value = 1;
+        }
+    }
+
+    public void SpawnTeamMember()
+    {
+        if (teamSlider.value <= .5)
+        {
+            Instantiate(team2Member, team2Spawn.position, Quaternion.identity);
+            teamSelectCanvas.enabled = false;
+            teamSelectCamera.enabled = false;
+        }
+        if (teamSlider.value > .5)
+        {
+            Instantiate(team1Member, team1Spawn.position, Quaternion.identity);
+            teamSelectCanvas.enabled = false;
+            teamSelectCamera.enabled = false;
+        }
+    }
+	
+}
