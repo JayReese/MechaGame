e2c192750eee4c68af29f66a7960a3f951b6be56
ChaRRoss <charlesjross92@gmail.com>
2017-05-18 17:37:51 -0500

    Update to Team Management
    adds some organization to teams in the hierarchy as well as debug team
    scoring. Adds a TempPlayer script (sorry for more scripts) inorder to
    tag each of the spawned players with  the appropriate team TAG.

--- Assets/Scripts/TeamScore.cs
+++ Assets/Scripts/TeamScore.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class TeamScore : MonoBehaviour {
+    [SerializeField]
+    Text Team1Score;
+    [SerializeField]
+    Text Team2Score;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public int Team1Kills = 0;
+    public int Team2Kills = 0;
+    // Use this for initialization
+    void Start () {
+        Team1Score.text = string.Format("Team 1 Score: 0");
+        Team2Score.text = string.Format("Team 2 Score: 0");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Team1Kills >= 10)
+        {
+            Debug.Log("Team 1 Wins");
+        }
+        else if (Team2Kills >= 10)
+        {
+            Debug.Log("Team 2 Wins");
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Team1Kills++;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Team2Kills++;
+        }
+
+        Team1Score.text = string.Format("Team 1 Score: {0}",Team1Kills);
+        Team2Score.text = string.Format("Team 2 Score: {0}",Team2Kills);
+    }
 }
