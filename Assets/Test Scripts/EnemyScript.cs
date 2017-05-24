be60ba1b7568bb641ffde0f94c9821ed540b16fe
3voker <DeAndre.Owens@iam.local>
2017-05-24 13:28:08 -0500

    PlayerHealthUI Player Ammo UI Enemy Health WorldSpace UI
    DeAndre - Added functions to Health, Ammo, and Enemy Health UI. Panels
    can be set to active and inactive in code. Need to rework enemy world
    space to proper proportions.

---
+++ Assets/Test Scripts/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class EnemyScript : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    Text playerAmmoText;
+    [SerializeField]
+    Text enemyHealthText;
+    [SerializeField]
+    GameObject HealthPanel;
+
+    int enemyHealth = 100;
+    float checkPointTextTime = 2.5f;
+    public float m_StartingHealth = 100f;
+    public float m_CurrentHealth;
+    private bool m_Dead;
+    private void TextBoxCheck()
+    {
+        if (HealthPanel.activeSelf)
+        {
+            checkPointTextTime = 1f;
+            checkPointTextTime -= Time.deltaTime;
+            enemyHealthText.text = enemyHealth.ToString();
+            if (checkPointTextTime <= 0)
+            {
+                playerAmmoText.text = "";
+                HealthPanel.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Test Scripts/PlayerScript.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class PlayerScript : TestGameManager
+{
+
+    // Use this for initialization
+    TestGameManager testGameManager;
+    [SerializeField]
+    Text playerAmmoText;
+    [SerializeField]
+    Text playerHealthText;
+    float checkPointTextTime = 2.5f;
+    //Variables private
+    float m_HorizontalInputValue;
+    float m_VerticalInputValue;
+    int playerHealth = 100;
+    bool isFiring = false;
+    int playerAmmo = 1000;
+
+    public float m_StartingHealth = 100f;
+    public Slider m_Slider;
+    public Image m_FillImage;
+    public Color m_FullHealthColor = Color.green;
+    public Color m_ZeroHealthColor = Color.red;
+    public GameObject m_ExplosionPrefab;
+    public GameObject Smoke;
+
+    public float m_CurrentHealth;
+    private bool m_Dead;
+
+    [SerializeField]
+    GameObject ammoTextPanel;
+    void Start()
+    {
+        TestGameManager testGameManager = GetComponent<TestGameManager>();
+    }
+    //Public variables go here.
+   
+    //public int m_PlayerNumber = 1;
+
+    void OnEnable()
+    {   
+        m_HorizontalInputValue = 0f;    
+        m_VerticalInputValue = 0f;
+    }
+    private void OnDisable()
+    {
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        m_HorizontalInputValue = Input.GetAxis("Horizontal");
+        m_VerticalInputValue = Input.GetAxis("Vertical");
+        SetHealthUI();
+        if (Input.GetButtonDown("Fire1"))
+            isFiring = true;
+        if (Input.GetButtonUp("Fire1"))
+            isFiring = false;
+            if (isFiring == true) //Input.GetButtonDown("JUmp")
+        {
+           
+            ammoTextPanel.SetActive(true);
+            FireWeapon();
+            Debug.Log("Fired weapon");
+            TextBoxCheck();
+        }
+        else if (isFiring == false)
+        {
+            //ammoTextPanel.SetActive(false);          
+        }
+    }
+    private void TextBoxCheck()
+    {
+        if (ammoTextPanel.activeSelf)
+        {
+            checkPointTextTime = 1f;
+            checkPointTextTime -= Time.deltaTime;
+            if (checkPointTextTime <= 0)
+            {
+                playerAmmoText.text = "";
+                ammoTextPanel.SetActive(false);
+            }
+        }
+    }
+
+void FireWeapon()
+{
+  // playerAmmo -= Time.deltaTime;
+
+    playerAmmo--;
+    playerAmmoText.text = playerAmmo.ToString();
+}
+    private void SetHealthUI()
+    {
+        playerHealthText.text = playerHealth.ToString();
+        // Adjust the value and colour of the slider.
+        //m_Slider.value = m_CurrentHealth;
+        //m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+    }
+    void TakeDamage()
+    {
+        playerHealth--;
+        playerHealthText.text = playerHealth.ToString();
+    }
+
+
+}
+
+
