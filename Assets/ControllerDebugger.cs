540ab96d7c3212b13f8967b9308af68fb74c9752
ChaRRoss <charlesjross92@gmail.com>
2017-05-24 09:47:04 -0500

    ControllerAxis set up
    Supports 4 controllers (Currently just their left joystick input)
    demonstrates the way the input should be read. Very easy to implement
    into playerController scripts by giving individuals identifier int!

--- Assets/ControllerDebugger.cs
+++ Assets/ControllerDebugger.cs
@@ -6,12 +6,25 @@ public class ControllerDebugger : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update ()
+#if UNITY_EDITOR
+        Debug.Log("This is Unity Editor");
+#endif
+#if UNITY_STANDALONE_WIN
+        Debug.Log("On Windows");
+#endif
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
-        
-	}
+        int i = 0;
+        while (i < 4)
+        {
+            if (Mathf.Abs(Input.GetAxis("Joy" + i + "X")) > 0.2F)
+                Debug.Log(Input.GetJoystickNames()[i] + i + " is moved in X");
+            if (Mathf.Abs(Input.GetAxis("Joy" + i + "Y")) > 0.2F)
+                Debug.Log(Input.GetJoystickNames()[i] + i + " is moved in Y");
+            i++;
+        }
+    }
 }
