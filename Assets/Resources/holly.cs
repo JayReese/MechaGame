536c221b7e6d17c61efb9af55e339dd2eb86f1f3
Dave M <dmajchrowicz414@gmail.com>
2017-06-26 18:33:21 -0500

    Added new test level.

---
+++ Assets/Resources/holly.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class holly : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameObject.GetComponent<MeshRenderer> ().material.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
