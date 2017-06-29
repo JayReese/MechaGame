fe66a949f3cdc9aa49c4018b5aa31f9740b7e900
CamHargrave <tvastacarious@gmail.com>
2017-06-29 14:02:37 -0500

    Mesh Colliders and Destructible env code attached

---
+++ Assets/Scripts/DestructibleEnvironment.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructibleEnvironment : DamageableObject
+{
+    private void Awake()
+    {
+        IsPersistingObject = false;
+        IsPlayer = false;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
