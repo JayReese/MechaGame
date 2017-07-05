ddb82e5dae911d5e51650af7761821541809f4c4
ChaRRoss <Charles.Ross@iam.local>
2017-07-05 15:31:17 -0500

    SpiderDroneDebug
    The functionality of the Spider Drone is for the most part done. The
    specifics might need to be changed based on drone type but the code is
    very reusable. Buildings require the tag "Building" and an empty
    gameobject as the position for the travel destination of the drones.

---
+++ Assets/Scripts/Spider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+public enum SpiderMechState { Spawning, Traveling, Armed, Detonated }
+
+public class Spider
+{
+    private SpiderMechState spiderstate;
+    public SpiderMechState spiderState
+    {
+        get { return spiderstate; }
+        private set { spiderstate = value; }
+    }
+
+    public Spider()
+    {
+        spiderstate = SpiderMechState.Spawning;
+    }
+
+    public void ChangeStateExt(SpiderMechState spState)
+    {
+        spiderstate = spState;
+    }
+
+}
---
+++ Assets/Scripts/SpiderController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderController : MonoBehaviour
+{
+    public GameObject[] buildings;
+    public GameObject ClosestBuilding;
+    Spider spider;
+    public Vector3 TargetDestination;
+    public float Speed;
+    // Use this for initialization
+    void Awake()
+    {
+        spider = new Spider();
+        buildings = GameObject.FindGameObjectsWithTag("Building");
+    }
+    void Start()
+    {
+        ClosestBuilding = buildings[0];
+        for (int i = 1; i < buildings.Length; i++)
+        {
+            if ((this.transform.position - ClosestBuilding.transform.position).magnitude >
+                (this.transform.position - buildings[i].transform.position).magnitude)
+            {
+                ClosestBuilding = buildings[i];
+            }
+        }
+        TargetDestination = ClosestBuilding.transform.GetChild(0).transform.position;
+        spider.ChangeStateExt(SpiderMechState.Traveling);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (spider.spiderState == SpiderMechState.Traveling)
+        {
+            if (Mathf.Abs(this.transform.position.x - TargetDestination.x) > .2)
+            {
+                //Debug.Log("Moving x");
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(TargetDestination.x, transform.position.y, transform.position.z), Speed * Time.deltaTime);
+            }
+            else if ((Mathf.Abs(this.transform.position.z - TargetDestination.z) > .2))
+            {
+                //Debug.Log("Moving z");
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(TargetDestination.x, transform.position.y, TargetDestination.z), Speed * Time.deltaTime);
+            }
+            else
+            {
+                //Debug.Log("Moving y");
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(TargetDestination.x, TargetDestination.y, TargetDestination.z), Speed * Time.deltaTime);
+            }
+            CheckForArrival();
+            Debug.Log(spider.spiderState);
+        }
+    }
+
+    private void CheckForArrival()
+    {
+        if ((this.transform.position - TargetDestination).magnitude <= .4)
+        {
+            spider.ChangeStateExt(SpiderMechState.Armed);
+            this.transform.position = TargetDestination;
+        }
+    }
+
+}
