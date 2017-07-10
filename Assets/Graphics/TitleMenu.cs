1ce51acbd01c15330ca23f1ec779943cc60a771c
Kuroshi117 <phoebehana4@gmail.com>
2017-07-10 16:17:58 -0500

    Work from 6.10
    TitleSelect prototype

---
+++ Assets/Graphics/TitleMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour {
+
+	[SerializeField]
+	public List<Button> Buttons = new List<Button>();
+	public int Selection;
+	public string selectionInput="Vertical";
+	public float selectionSensitivity;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		Selection = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown(selectionInput)) 
+		{
+			if (Input.GetAxis (selectionInput) > 0) 
+			{
+				Selection--;
+			}
+
+			if (Input.GetAxis (selectionInput) < 0) 
+			{
+				Selection++;
+			}
+
+			if (Selection < 0) 
+			{
+				Selection = 2;
+			}
+			if (Selection > 2) 
+			{
+				Selection = 0;
+			}
+		}
+
+	}
+}
